b38779534fc5c84d188ba5deb44e09710aa054db
Mauricio <mauricio.guzman.salazar@gmail.com>
2019-08-07 14:51:02 -0500

    Initial Commit

---
+++ infinitehorizontalscroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  infinitehorizontalscroll
+//
+//  Created by Mauricio Guzmán Salazar on 8/7/19.
+//  Copyright © 2019 Mauricio Guzmán Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
